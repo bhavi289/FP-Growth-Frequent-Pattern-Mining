044c2a68383d7345cee32bcef69fc10ec356064e
bhavi289 <bhavi.c16@iiits.in>
2018-08-20 15:14:51 +0530

    seperting items from csv

--- FP-Growth-Frequent-Pattern-Mining.c
+++ FP-Growth-Frequent-Pattern-Mining.c
@@ -1,16 +1,88 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void readFile(char fileName[]){
+typedef struct HeaderTable{
+	char item[10000];
+	int frequency;
+	// pointer to tree node
+}HEADERTABLE;
+
+void AddInHeaderTable(char transaction[], HEADERTABLE* headerTable, int row){
+	/*int i, flag = 0, index = 0;
+	// strcpy(headerTable[row].item, transaction);
+	char insertString[50];
+	for(i=0; i<strlen(transaction); i++){
+		insertString[index] = transaction[i];
+		if (transaction[i] == ',' && flag == 0){
+			flag = 1; 
+			index = 0;
+			continue;	
+		}
+		if(transaction[i] == ','){
+			insertString[index] = '\0';
+			printf("%s\n", insertString);
+			strcpy(headerTable[row], insertString);
+			index = 0;
+		}
+		index = index + 1;
+	}
+	printf("\n");*/
+}
+
+void ReadFile(char fileName[], HEADERTABLE* headerTable){
 	FILE *fp;
-	char name[10000];
+	char transaction[10000], allEntries[5000][30];
 	fp = fopen(fileName, "r");
+	int row = 0, i;
+	int total = 0;
 	do{
-        fscanf(fp," %[^\n]s",name);
-        printf("%s\n", name);
+		char insertString[100];
+		insertString[0] = '\0';
+
+		int flag = 0;
+		int index = 0;
+        fscanf(fp," %[^\n]s",transaction);
+        strcat(transaction,",");
+        for(i=0; i<strlen(transaction); i++){
+			if (transaction[i] == ',' && flag == 0){
+				flag = 1; 
+			}
+			else if (transaction[i] == ',' && transaction[i-1] == ','){
+				char insertString[50];
+				insertString[0] = '\0';
+				break;
+			}
+			else if (transaction[i] == ',') {
+				insertString[index] = '\0';
+				// printf("%s\n", insertString);
+				strcpy(allEntries[total],insertString);
+				// printf("%s\n", insertString);
+
+				// strcpy(headerTable[total].item, insertString);
+				index = 0;
+			    // printf("%d\n", total );
+				total += 1;
+				char insertString[50];
+			}
+			else if (flag == 1){
+				insertString[index] = transaction[i];
+				index = index + 1;
+			}
+		}
+		
+        row = row + 1;
+        // if(row == 4) break;
     }
     while(!feof(fp));
+    for(i=0; i<total; i++)
+			printf("%d %s\n",i, allEntries[i]);
+    printf("----------------%d\n", total );
 	fclose(fp);
 }
+
 int main(){
-	readFile("groceries_subset.csv");
+	HEADERTABLE* headerTable = (HEADERTABLE *)malloc(100000 * sizeof(HEADERTABLE));
+	ReadFile("groceries_subset.csv", headerTable);
+	printf("%s\n", headerTable[1].item );
 }
