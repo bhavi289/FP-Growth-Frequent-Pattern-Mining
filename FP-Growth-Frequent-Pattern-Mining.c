eca11c5a49ac397c188fc3246889f2a0d89b4e20
bhavi289 <bhavi.c16@iiits.in>
2018-08-21 00:32:23 +0530

    Creating Header Table - sorted descending order, min support=0.05

--- FP-Growth-Frequent-Pattern-Mining.c
+++ FP-Growth-Frequent-Pattern-Mining.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+# define SUPPORT 0.05
 typedef struct HeaderTable{
 	char item[10000];
 	int frequency;
 	// pointer to tree node
 }HEADERTABLE;
 
-void AddInHeaderTable(char transaction[], HEADERTABLE* headerTable, int row){
-	/*int i, flag = 0, index = 0;
-	// strcpy(headerTable[row].item, transaction);
-	char insertString[50];
-	for(i=0; i<strlen(transaction); i++){
-		insertString[index] = transaction[i];
-		if (transaction[i] == ',' && flag == 0){
-			flag = 1; 
-			index = 0;
-			continue;	
-		}
-		if(transaction[i] == ','){
-			insertString[index] = '\0';
-			printf("%s\n", insertString);
-			strcpy(headerTable[row], insertString);
-			index = 0;
-		}
-		index = index + 1;
-	}
-	printf("\n");*/
+int cmpfunc (const void * a, const void * b) {
+   return ( (*(HEADERTABLE*)b).frequency - (*(HEADERTABLE*)a).frequency );
 }
 
-void ReadFile(char fileName[], HEADERTABLE* headerTable){
+int AddInHeaderTable(HEADERTABLE *temporaryTable, char insertString[]){
+	static int index = 0;
+	int i = 0, flag = 0;
+	// printf("%s\n", insertString);
+	for(int i = 0; i<index; i++){
+		if (strcmp(temporaryTable[i].item,insertString) == 0){
+			temporaryTable[i].frequency += 1;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0){
+		strcpy(temporaryTable[index].item, insertString);
+		temporaryTable[index].frequency = 1;
+		index = index + 1;
+	}
+	return index;
+}
+
+int ReadFile(char fileName[], HEADERTABLE* headerTable){
 	FILE *fp;
+	HEADERTABLE* temporaryTable = (HEADERTABLE *)malloc(100000 * sizeof(HEADERTABLE));
 	char transaction[10000], allEntries[5000][30];
 	fp = fopen(fileName, "r");
 	int row = 0, i;
-	int total = 0;
-	do{
+	int total = 0, lengthOfTable = 0;
+	while(fscanf(fp," %[^\n]s",transaction) == 1){
 		char insertString[100];
 		insertString[0] = '\0';
-
 		int flag = 0;
 		int index = 0;
-        fscanf(fp," %[^\n]s",transaction);
         strcat(transaction,",");
         for(i=0; i<strlen(transaction); i++){
 			if (transaction[i] == ',' && flag == 0){
@@ -55,13 +55,8 @@ void ReadFile(char fileName[], HEADERTABLE* headerTable){
 			}
 			else if (transaction[i] == ',') {
 				insertString[index] = '\0';
-				// printf("%s\n", insertString);
-				strcpy(allEntries[total],insertString);
-				// printf("%s\n", insertString);
-
-				// strcpy(headerTable[total].item, insertString);
+				lengthOfTable = AddInHeaderTable(temporaryTable, insertString);
 				index = 0;
-			    // printf("%d\n", total );
 				total += 1;
 				char insertString[50];
 			}
@@ -70,19 +65,29 @@ void ReadFile(char fileName[], HEADERTABLE* headerTable){
 				index = index + 1;
 			}
 		}
-		
         row = row + 1;
         // if(row == 4) break;
     }
-    while(!feof(fp));
-    for(i=0; i<total; i++)
-			printf("%d %s\n",i, allEntries[i]);
-    printf("----------------%d\n", total );
+
+    printf("%d\n", lengthOfTable);
+    qsort(temporaryTable, lengthOfTable, sizeof(HEADERTABLE), cmpfunc);
+    int supportThreshold = SUPPORT * row;
+    printf("supportThreshold %d\n", supportThreshold );
+	for ( i=0; i<lengthOfTable; i++ ){
+		if(temporaryTable[i].frequency > supportThreshold)
+			headerTable[i] = temporaryTable[i];
+		else break;
+    }
 	fclose(fp);
+	free(temporaryTable);
+	return i;
 }
 
 int main(){
 	HEADERTABLE* headerTable = (HEADERTABLE *)malloc(100000 * sizeof(HEADERTABLE));
-	ReadFile("groceries_subset.csv", headerTable);
-	printf("%s\n", headerTable[1].item );
+	int lengthOfTable, i;
+	lengthOfTable = ReadFile("groceries_subset.csv", headerTable);
+	for ( i=0; i<lengthOfTable; i++ ){
+		printf("%s %d\n", headerTable[i].item, headerTable[i].frequency);
+	}
 }
